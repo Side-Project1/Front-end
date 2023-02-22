2b55eb1c71e38a2dda92b34cfb1cf60cf3189d64
KwonSuyeon <commetoi_yeoni@naver.com>
2023-02-23 02:05:39 +0900

    app.js에 loginPage 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./constant/Header";
+import Login from "./constant/Login";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Login />
       </div>
     </BrowserRouter>
   );
