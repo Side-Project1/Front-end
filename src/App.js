0e9ee18ed74afefe015a8f1deae3465fde46c9db
KwonSuyeon <commetoi_yeoni@naver.com>
2023-02-24 02:41:45 +0900

    feat: joinPage ui 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./constant/Header";
+import Join from "./constant/Join";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Join />
       </div>
     </BrowserRouter>
   );
---
+++ src/constant/Join.style.js
@@ -0,0 +1,150 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: #a7bffd;
+`;
+
+export const SubWrapper = styled.div``;
+
+export const LeftSection = styled.div``;
+
+export const TopButton = styled.button`
+  color: #032363;
+  background: #cfdcfe;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 110px;
+  border-style: none;
+
+  &:active {
+    background: #032363;
+    color: #ffffff;
+  }
+`;
+
+export const BottomButton = styled.button`
+  color: #032363;
+  background: #cfdcfe;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 110px;
+  border-style: none;
+
+  &:active {
+    background: #032363;
+    color: #ffffff;
+  }
+`;
+
+export const NextButton = styled.button`
+  background: #2f406e;
+  border-radius: 33px;
+  color: #ffffff;
+`;
+
+export const RightSection = styled.div``;
+
+export const ButtonContainer = styled.div``;
+
+export const LeftButton = styled.button`
+  color: #032363;
+  background: #cddbff;
+  box-shadow: inset 0px 4px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 18px;
+  border-style: none;
+
+  &:active {
+    background: #032363;
+    color: #ffffff;
+  }
+`;
+
+export const RightButton = styled.button`
+  color: #032363;
+  background: #cddbff;
+  box-shadow: inset 0px 4px 20px rgba(0, 0, 0, 0.1);
+  border-radius: 18px;
+  border-style: none;
+
+  &:active {
+    background: #032363;
+    color: #ffffff;
+  }
+`;
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 37px;
+`;
+
+export const FormTitle = styled.div`
+  width: 104px;
+  color: #fff;
+`;
+
+export const FormBox = styled.div`
+  display: flex;
+`;
+
+export const Name = styled.input`
+  width: 309px;
+  height: 31px;
+  border-style: none;
+`;
+
+export const Id = styled.input`
+  width: 309px;
+  height: 31px;
+  border-style: none;
+`;
+
+export const Password = styled.input`
+  width: 309px;
+  border-style: none;
+  height: 31px;
+`;
+
+export const Number = styled.input`
+  width: 309px;
+  border-style: none;
+  height: 31px;
+`;
+
+export const Email = styled.input`
+  width: 309px;
+  height: 31px;
+  border-style: none;
+`;
+
+export const Birth = styled.input`
+  width: 309px;
+  height: 31px;
+  border-style: none;
+`;
+
+export const SexButton = styled.div``;
+
+export const MaleButton = styled.button`
+  color: #032363;
+  background: #dce6ff;
+  opacity: 0.9;
+  border-radius: 18px;
+  border-style: none;
+
+  &:active {
+    background: #032363;
+    color: #ffffff;
+  }
+`;
+
+export const FemaleButton = styled.button`
+  color: #032363;
+  background: #dce6ff;
+  opacity: 0.9;
+  border-radius: 18px;
+  border-style: none;
+
+  &:active {
+    background: #032363;
+    color: #ffffff;
+  }
+`;
