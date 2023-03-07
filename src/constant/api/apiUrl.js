50a67d382b165bda9ec6844be31e75514e3ed429
KwonSuyeon <commetoi_yeoni@naver.com>
2023-03-07 14:50:08 +0900

    feat: api url 추가

--- src/constant/api/apiUrl.js
+++ src/constant/api/apiUrl.js
@@ -1,2 +1,9 @@
+/*Auth*/
 export const POST_LOGIN = "/auth/login";
 export const POST_SIGN_UP = "/auth/signup";
+export const GET_REFRESHTOKEN = "/atuh/refreshtoken/{userId}";
+
+/*Email*/
+export const GET_CONFIRM_EMAIL = "/api/v1/email/confirm";
+export const POST_FIND_EMAIL = "/api/v1/email/findId";
+export const POST_SEND_EMAIL = "/api/v1/email/send";
