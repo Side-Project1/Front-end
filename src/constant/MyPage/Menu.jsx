6d856b4a10e0565929e65a94a61ba3325fe1fbac
KwonSuyeon <commetoi_yeoni@naver.com>
2023-03-28 02:55:34 +0900

    fix: map 함수 사용해서 코드 리팩토링

--- src/constant/MyPage/Menu.jsx
+++ src/constant/MyPage/Menu.jsx
@@ -6,30 +6,12 @@ const Menu = () => {
     <S.Wrapper>
       <S.Container>
         <S.MenuSection>
-          <S.Section>
-            <S.MenuIcon src="/image/MyPage/folder.svg"></S.MenuIcon>
-            <S.Title>프로필</S.Title>
-          </S.Section>
-          <S.Section>
-            <S.MenuIcon src="/image/MyPage/Wrote.svg"></S.MenuIcon>
-            <S.Title>내가 쓴 글</S.Title>
-          </S.Section>
-          <S.Section>
-            <S.MenuIcon src="/image/MyPage/Comment.svg"></S.MenuIcon>
-            <S.Title>내가 쓴 댓글</S.Title>
-          </S.Section>
-          <S.Section>
-            <S.MenuIcon src="/image/MyPage/Pin.svg"></S.MenuIcon>
-            <S.Title>스크랩</S.Title>
-          </S.Section>
-          <S.Section>
-            <S.MenuIcon src="/image/MyPage/Doc.svg"></S.MenuIcon>
-            <S.Title>이력서</S.Title>
-          </S.Section>
-          <S.Section>
-            <S.MenuIcon src="/image/MyPage/Support.svg"></S.MenuIcon>
-            <S.Title>지원현황</S.Title>
-          </S.Section>
+          {MY_PAGE_MENU.map((data, index) => (
+            <S.Section key={index}>
+              <S.MenuIcon src={data.icon} />
+              <S.Title>{data.title}</S.Title>
+            </S.Section>
+          ))}
         </S.MenuSection>
       </S.Container>
     </S.Wrapper>
