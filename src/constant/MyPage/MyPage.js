4b71371244a4c50aed177b4584d4cfc96d5e6ac1
KwonSuyeon <commetoi_yeoni@naver.com>
2023-03-28 02:40:54 +0900

    feat: MyPage Menu List 추가

---
+++ src/constant/MyPage/MyPage.js
@@ -0,0 +1,26 @@
+export const MY_PAGE_MENU = [
+  {
+    icon: "/image/MyPage/folder.svg",
+    title: "프로필",
+  },
+  {
+    icon: "/image/MyPage/Wrote.svg",
+    title: "내가 쓴 글",
+  },
+  {
+    icon: "/image/MyPage/Comment.svg",
+    title: "내가 쓴 댓글",
+  },
+  {
+    icon: "/image/MyPage/Pin.svg",
+    title: "스크랩",
+  },
+  {
+    icon: "/image/MyPage/Doc.svg",
+    title: "이력서",
+  },
+  {
+    icon: "/image/MyPage/Support.svg",
+    title: "지원현황",
+  },
+];
