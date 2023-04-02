4b838d4effaf47569148d667065c8c0003eafd51
KwonSuyeon <commetoi_yeoni@naver.com>
2023-04-03 02:48:09 +0900

    feat: JoinPage UI 프레임 구현

--- src/constant/Join.style.js
+++ src/constant/Join.style.js
@@ -1,218 +1,61 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-  background-color: #a7bffd;
+  background: left bottom url("/image/ArtNJob_Boy.png");
+  background-size: 937px 673px;
+  background-color: #fcfbe9;
+  background-repeat: no-repeat;
 `;
 
 export const SubWrapper = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
-  padding-left: 110px;
-  padding-right: 110px;
-  padding-bottom: 200px;
+  background: #383838;
+  background-color: rgba(0, 0, 0, 0.3);
+  padding: 47px 81px 60px;
 `;
 
-export const Title = styled.div`
-  font-weight: 400;
-  font-size: 150px;
-  line-height: 106%;
-  color: rgba(255, 255, 255, 0.5);
-  text-align: start;
-  margin-bottom: 40px;
+export const LeftContainer = styled.div``;
+
+export const TitleBox = styled.div`
+  margin-top: 65px;
+  width: 555px;
+  height: 198px;
 `;
 
-export const LeftSection = styled.div`
+export const TopTitle = styled.img`
   display: flex;
-  flex-direction: column;
-  margin-top: 85px;
+  justify-content: flex-start;
+  width: 210px;
+  height: 66px;
 `;
 
-export const TopButton = styled.button`
-  width: 274px;
-  height: 68px;
+export const BottomTitle = styled.div`
+  font-family: "Inter";
+  font-style: normal;
   font-weight: 500;
   font-size: 17px;
   line-height: 160%;
-  color: #032363;
-  background: #cfdcfe;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 110px;
-  border-style: none;
-  cursor: pointer;
-  margin-bottom: 60px;
-
-  &:active {
-    background: #032363;
-    color: #ffffff;
-  }
-`;
-
-export const BottomButton = styled.button`
-  width: 274px;
-  height: 68px;
-  font-weight: 500;
-  font-size: 17px;
-  line-height: 160%;
-  color: #032363;
-  background: #cfdcfe;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 110px;
-  border-style: none;
-  cursor: pointer;
-  margin-bottom: 321px;
-
-  &:active {
-    background: #032363;
-    color: #ffffff;
-  }
-`;
-
-export const NextButton = styled.button`
-  width: 274px;
-  height: 68px;
-  font-weight: 500;
-  font-size: 17px;
-  line-height: 160%;
-  background: #2f406e;
-  border-radius: 33px;
+  letter-spacing: -0.02em;
   color: #ffffff;
-  border-style: none;
-  cursor: pointer;
+  text-align: start;
 `;
 
-export const RightSection = styled.div``;
+export const RightContainer = styled.div`
+  font-size: 32px;
+  line-height: 100%;
+  color: #181818;
+`;
 
-export const ButtonContainer = styled.div`
+export const Box = styled.div`
   display: flex;
-  margin-bottom: 67px;
-  column-gap: 37px;
-`;
-
-export const LeftButton = styled.button`
-  width: 232px;
-  height: 59px;
-  color: #032363;
-  background: #cddbff;
-  box-shadow: inset 0px 4px 20px rgba(0, 0, 0, 0.1);
-  border-radius: 18px;
-  border-style: none;
-  cursor: pointer;
-
-  &:active {
-    background: #032363;
-    color: #ffffff;
-  }
-`;
-
-export const RightButton = styled.button`
-  width: 232px;
-  height: 59px;
-  color: #032363;
-  background: #cddbff;
-  box-shadow: inset 0px 4px 20px rgba(0, 0, 0, 0.1);
-  border-radius: 18px;
-  border-style: none;
-  cursor: pointer;
-
-  &:active {
-    background: #032363;
-    color: #ffffff;
-  }
-`;
-
-export const FormContainer = styled.div`
-  display: flex;
+  justify-content: center;
   flex-direction: column;
-  row-gap: 37px;
-`;
-
-export const FormTitle = styled.div`
-  width: 104px;
-  color: #fff;
-`;
-
-export const FormBox = styled.div`
-  display: flex;
-  justify-content: end;
-`;
-
-export const Name = styled.input`
-  width: 309px;
-  height: 31px;
-  border-style: none;
-`;
-
-export const Id = styled.input`
-  width: 309px;
-  height: 31px;
-  border-style: none;
-`;
-
-export const PasswordBox = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 37px;
-`;
-
-export const Password = styled.input`
-  width: 309px;
-  border-style: none;
-  height: 31px;
-`;
-
-export const Number = styled.input`
-  width: 309px;
-  border-style: none;
-  height: 31px;
-`;
-
-export const Email = styled.input`
-  width: 309px;
-  height: 31px;
-  border-style: none;
-`;
-
-export const Birth = styled.input`
-  width: 309px;
-  height: 31px;
-  border-style: none;
-`;
-
-export const SexButton = styled.div`
-  display: flex;
-  justify-content: end;
-  column-gap: 20px;
-`;
-
-export const MaleButton = styled.button`
-  width: 85px;
-  height: 32px;
-  color: #032363;
-  background: #dce6ff;
-  opacity: 0.9;
-  border-radius: 18px;
-  border-style: none;
-  cursor: pointer;
-
-  &:active {
-    background: #032363;
-    color: #ffffff;
-  }
-`;
-
-export const FemaleButton = styled.button`
-  width: 85px;
-  height: 32px;
-  color: #032363;
-  background: #dce6ff;
-  opacity: 0.9;
-  border-radius: 18px;
-  border-style: none;
-  cursor: pointer;
-
-  &:active {
-    background: #032363;
-    color: #ffffff;
-  }
+  width: 639px;
+  height: 865px;
+  background: #ffffff;
+  border-radius: 42px;
+  width: 415px;
+  padding: 0px 111px;
 `;
