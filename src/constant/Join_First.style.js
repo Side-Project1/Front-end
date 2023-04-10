8201499c66245c8e914f03c41fbb8c5e6ffc613d
KwonSuyeon <commetoi_yeoni@naver.com>
2023-04-11 03:06:50 +0900

    feat: join Page 병합

--- src/constant/Join_First.style.js
+++ src/constant/Join_First.style.js
@@ -1,3 +1,4 @@
+import { style } from "@mui/system";
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
@@ -29,6 +30,59 @@ export const TopTitle = styled.img`
   justify-content: flex-start;
   width: 210px;
   height: 66px;
+`;
+
+export const Arrow = styled.img`
+  width: calc(100% + 24 * 2);
+  margin: 0px 37px 0px -74px;
+  cursor: pointer;
+`;
+
+export const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 15px;
+`;
+
+export const Label = styled.div`
+  font-weight: 500;
+  font-size: 13.5px;
+  line-height: 160%;
+  align-items: center;
+  letter-spacing: -0.02em;
+  color: #9f9f9f;
+`;
+
+export const DropDown = styled.img`
+  margin-right: 30px;
+`;
+
+export const SelectOptions = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  background: #ffffff;
+  border: 1px solid #d0d0d0;
+  border-radius: 11px;
+  margin: 26px 0px 0px -40px;
+  padding: 4px 4px;
+`;
+
+export const Option = styled.div`
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 160%;
+  text-align: center;
+  letter-spacing: -0.02em;
+  color: #262626;
+  width: 185px;
+  height: 38px;
+
+  &:hover {
+    background: #f58425;
+    color: #ffffff;
+    border-radius: 4px;
+  }
 `;
 
 export const BottomTitle = styled.div`
@@ -99,13 +153,14 @@ export const Name = styled.input`
   padding-left: 44px;
 `;
 
-export const Gender = styled.input`
+export const Gender = styled.div`
   width: 157px;
   height: 54px;
   background: #ffffff;
   border: 1px solid #d0d0d0;
   border-radius: 21px;
   padding-left: 44px;
+  cursor: pointer;
 `;
 
 export const UserId = styled.input`
@@ -174,4 +229,25 @@ export const NextButton = styled.button`
   color: #2f2f2f;
   margin-top: 27px;
   cursor: pointer;
+`;
+
+export const Agreement = styled.div`
+  width: 415px;
+  height: 186px;
+  background: #ffffff;
+  border: 1px solid #d0d0d0;
+  border-radius: 21px;
+`;
+
+export const NextBox = styled.div`
+  display: none;
+  gap: 9px;
+  justify-content: center;
+  flex-direction: column;
+  width: 639px;
+  height: 865px;
+  background: #ffffff;
+  border-radius: 42px;
+  width: 415px;
+  padding: 0px 111px;
 `;
