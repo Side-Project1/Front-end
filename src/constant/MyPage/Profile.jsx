8ef0ec86ac9e712f8859ba51251967411e67a2e2
KwonSuyeon <commetoi_yeoni@naver.com>
2023-04-18 23:12:22 +0900

    add: profile Page Layout

--- src/constant/MyPage/Profile.jsx
+++ src/constant/MyPage/Profile.jsx
@@ -14,7 +14,41 @@ const Profile = () => {
           </S.SubTitle>
           <S.RequiredNotice>*필수입력항목</S.RequiredNotice>
         </S.TopSection>
-        <S.BottomSection></S.BottomSection>
+        <S.BottomSection>
+          <S.PhotoBox>
+            <S.Icon></S.Icon>
+            <S.Text>사진</S.Text>
+            <S.ImageDelete>사진 삭제</S.ImageDelete>
+            <S.ImageButton>사진 불러오기</S.ImageButton>
+          </S.PhotoBox>
+          <S.PhoneNumberBox>
+            <S.Icon></S.Icon>
+            <S.Text>휴대번호</S.Text>
+            <S.InputBoxText>휴대번호</S.InputBoxText>
+            <S.PhoneNumberInput placeholder="000-0000-0000"></S.PhoneNumberInput>
+          </S.PhoneNumberBox>
+          <S.AccountBox>
+            <S.Icon></S.Icon>
+            <S.Text>계정</S.Text>
+            <S.InputBoxText>이름</S.InputBoxText>
+            <S.LastNameInputBox placeholder="성"></S.LastNameInputBox>
+            <S.FirstNameInputBox placeholder="이름"></S.FirstNameInputBox>
+            <S.InputBoxText>이메일</S.InputBoxText>
+            <S.EmailInputBox placeholder="ss972972@naver.com"></S.EmailInputBox>
+            <S.InputBoxText>아이디</S.InputBoxText>
+            <S.EmailInputBox placeholder="ss97272"></S.EmailInputBox>
+          </S.AccountBox>
+          <S.PasswordBox>
+            <S.Icon></S.Icon>
+            <S.Text>비밀번호</S.Text>
+            <S.InputBoxText>현재 비밀번호</S.InputBoxText>
+            <S.PasswordInputBox></S.PasswordInputBox>
+            <S.InputBoxText>새 비밀번호</S.InputBoxText>
+            <S.PasswordInputBox></S.PasswordInputBox>
+            <S.InputBoxText>새 비밀번호 확인</S.InputBoxText>
+            <S.PasswordInputBox></S.PasswordInputBox>
+          </S.PasswordBox>
+        </S.BottomSection>
       </S.Container>
     </S.Wrapper>
   );
--- src/constant/MyPage/Profile.style.js
+++ src/constant/MyPage/Profile.style.js
@@ -4,7 +4,10 @@ export const Wrapper = styled.div`
   display: flex;
 `;
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  margin-left: 342px;
+`;
 
 export const MenuSection = styled.div``;
 
@@ -38,3 +41,69 @@ export const RequiredNotice = styled.div`
 `;
 
 export const BottomSection = styled.div``;
+
+export const Icon = styled.div``;
+
+export const Text = styled.div`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 28px;
+  color: #2d2d2d;
+  opacity: 0.6;
+`;
+
+export const ImageDelete = styled.button`
+  width: 107px;
+  height: 33px;
+  background: rgba(255, 198, 66, 0.8);
+  opacity: 0.8;
+  border-radius: 20px;
+`;
+
+export const ImageButton = styled.button`
+  width: 107px;
+  height: 33px;
+  background: #fcfbe9;
+  opacity: 0.8;
+  border: 1px solid #d3be00;
+  border-radius: 20px;
+`;
+
+export const PhotoBox = styled.div`
+  width: 447px;
+  height: 255px;
+  opacity: 0.8;
+  border: 1.5px solid #d3be00;
+  border-radius: 20px;
+`;
+export const PhoneNumberBox = styled.div`
+  width: 318px;
+  height: 292px;
+  opacity: 0.8;
+  border: 1.5px solid #d3be00;
+  border-radius: 20px;
+`;
+export const AccountBox = styled.div`
+  width: 447px;
+  height: 427px;
+  opacity: 0.8;
+  border: 1.5px solid #d3be00;
+  border-radius: 20px;
+`;
+export const PasswordBox = styled.div`
+  width: 318px;
+  height: 392px;
+  opacity: 0.8;
+  border: 1.5px solid #d3be00;
+  border-radius: 20px;
+`;
+
+export const InputBoxText = styled.div``;
+export const PhoneNumberInput = styled.input``;
+
+export const LastNameInputBox = styled.input``;
+
+export const FirstNameInputBox = styled.input``;
+
+export const EmailInputBox = styled.input``;
+export const PasswordInputBox = styled.input``;
