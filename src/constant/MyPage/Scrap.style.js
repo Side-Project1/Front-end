551d049539a4d096dcbeed6cff2d59da95265a35
KwonSuyeon <commetoi_yeoni@naver.com>
2023-04-23 23:45:58 +0900

    feat: MyPage Scrap ui

---
+++ src/constant/MyPage/Scrap.style.js
@@ -0,0 +1,170 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div``;
+
+export const Container = styled.div`
+  display: flex;
+  margin-left: 342px;
+  flex-direction: column;
+  margin-bottom: 91px;
+`;
+
+export const TopSection = styled.div``;
+
+export const Title = styled.div`
+  font-weight: 600;
+  font-size: 30px;
+  line-height: 36px;
+  color: #000000;
+  text-align: start;
+`;
+
+export const SubTitle = styled.div`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 28px;
+  color: #000000;
+`;
+
+export const BottomSection = styled.div``;
+
+export const TopBox = styled.div`
+  display: flex;
+  align-items: center;
+  max-width: 800px;
+`;
+
+export const AllSelect = styled.div``;
+
+export const Delete = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 28px;
+  color: #000000;
+  opacity: 0.6;
+`;
+
+export const AllItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 600;
+  font-size: 12px;
+  line-height: 15px;
+  color: #4d4501;
+  background: #fcfbe9;
+  opacity: 0.8;
+  border: 1px solid #d3be00;
+  border-radius: 20px;
+  width: 117px;
+  height: 33px;
+`;
+
+export const BottomBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const List = styled.div`
+  display: flex;
+  /* align-items: center; */
+  flex-direction: column;
+`;
+
+export const ListValue = styled.div`
+  display: flex;
+  align-items: center;
+  height: 132px;
+`;
+
+export const Bar = styled.div`
+  width: 801px;
+  height: 2px;
+  background: #edecdd;
+`;
+
+export const TitleValue = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 3px;
+  width: 271px;
+  margin-right: 106px;
+`;
+export const CheckBox = styled.div`
+  width: 13px;
+  height: 13px;
+  border: 1px solid rgba(0, 0, 0, 0.31);
+  border-radius: 50px;
+  margin-right: 19px;
+`;
+export const ListTitle = styled.div`
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #000000;
+  text-align: start;
+`;
+export const ListSubTitle = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 28px;
+  color: #000000;
+  opacity: 0.6;
+  text-align: start;
+`;
+export const ListDate = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 28px;
+  text-align: center;
+  color: #000000;
+  opacity: 0.6;
+  margin-right: 62px;
+  width: 75px;
+`;
+export const RecruitButton = styled.button`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 28px;
+  text-align: center;
+  color: #000000;
+  opacity: 0.6;
+  background: #ede59b;
+  opacity: 0.8;
+  border-radius: 20px;
+  border-style: none;
+  width: 97px;
+  height: 33px;
+  margin-right: 49px;
+`;
+export const Date = styled.div`
+  display: flex;
+  flex-direction: column;
+  row-gap: 6px;
+  width: 75px;
+  align-items: center;
+`;
+
+export const DateButton = styled.button`
+  font-weight: 500;
+  font-size: 9px;
+  line-height: 28px;
+  color: #ff0000;
+  opacity: 0.6;
+  border: 1px solid #ff6363;
+  border-radius: 20px;
+  width: 35px;
+  height: 19px;
+  display: flex;
+  align-items: center;
+  padding-top: 4px;
+`;
+
+export const DeadLine = styled.div`
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 28px;
+  text-align: center;
+  color: #000000;
+  opacity: 0.6;
+`;
