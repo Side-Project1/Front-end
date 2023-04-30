236f16d0e8eb3a7600b1ffbff822d8f5de7ee205
KwonSuyeon <commetoi_yeoni@naver.com>
2023-04-30 22:52:27 +0900

    feat: reviewPage ui 구현

---
+++ src/constant/MainPage/Review.jsx
@@ -0,0 +1,43 @@
+import * as S from "./Review.style";
+
+const Review = () => {
+  return (
+    <S.Wrapper>
+      <S.Container>
+        <S.TopSection>
+          <S.Title>전/현직자들의 생생한 피드백</S.Title>
+          <S.SubTitle>옆으로 넘겨서 많은 리뷰들을 확인하세요!</S.SubTitle>
+        </S.TopSection>
+        <S.BottomSection>
+          <S.Card>
+            <S.CardBox>
+              <S.Box>
+                <S.Icon src="/image/icon/goodIcon.svg" />
+                <S.IconText>장점</S.IconText>
+              </S.Box>
+              <S.Box>
+                <S.Icon src="/image/icon/badIcon.svg" />
+                <S.IconText>단점</S.IconText>
+              </S.Box>
+              <S.Bar></S.Bar>
+              <S.Text>
+                이전 회사에서 네트워크 보안 솔루션을 개발하는 일을 하고
+                있었습니다. 네트워크 관련 분야에서 가장 뜨거운 주제가
+                클라우드였고, 자연스럽게 관심을 가지게 되었습니다. 한 번은 꼭
+                경험하고 싶어서 적극적으로 클라우드 서비스를 운영하는 기업을
+                알아보던 중에 제가 가진 기술과 시너지 효과를 낼 수 있을 만한
+                회사를 선택했습니다.
+              </S.Text>
+            </S.CardBox>
+            <S.Writer>
+              <S.WriterImg src="/image/icon/writerIcon.png" />
+              <S.WriterName>닉네임</S.WriterName>
+            </S.Writer>
+          </S.Card>
+        </S.BottomSection>
+      </S.Container>
+    </S.Wrapper>
+  );
+};
+
+export default Review;
---
+++ src/constant/MainPage/Review.style.js
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div``;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-top: 40px;
+  padding-bottom: 70px;
+  background: linear-gradient(180deg, #efefef 0%, rgba(239, 239, 239, 0) 100%);
+`;
+
+export const TopSection = styled.div`
+  margin-bottom: 122px;
+`;
+
+export const Title = styled.div`
+  font-weight: 700;
+  font-size: 24px;
+  line-height: 149%;
+  letter-spacing: -0.02em;
+  color: #484848;
+`;
+export const SubTitle = styled.div`
+  font-weight: 500;
+  font-size: 13px;
+  line-height: 150%;
+  letter-spacing: -0.03em;
+  color: #929292;
+`;
+
+export const BottomSection = styled.div`
+  align-self: center;
+`;
+
+export const CardBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Card = styled.div`
+  padding: 22px;
+  width: 394px;
+  height: 545px;
+  background: #ffffff;
+  border: 1px solid #c5c5c5;
+  box-shadow: 0px 4px 22px rgba(0, 0, 0, 0.2);
+  border-radius: 15px;
+`;
+
+export const Icon = styled.img``;
+
+export const IconText = styled.div`
+  font-weight: 700;
+  font-size: 15px;
+  line-height: 176.52%;
+  letter-spacing: -0.03em;
+  color: #323232;
+`;
+
+export const Bar = styled.div`
+  width: 394px;
+  height: 0px;
+  border: 1px solid rgba(105, 105, 105, 0.15);
+  margin: 26px 0px;
+`;
+
+export const Text = styled.div`
+  display: flex;
+  text-align: start;
+  width: 394px;
+  font-weight: 200;
+  font-size: 15px;
+  line-height: 176.52%;
+  letter-spacing: -0.03em;
+  color: #636363;
+`;
+
+export const Writer = styled.div`
+  display: flex;
+  gap: 18px;
+`;
+
+export const WriterImg = styled.img`
+  width: 55px;
+  height: 57px;
+  background-color: pink;
+  border: 1px solid #dadada;
+  border-radius: 50px;
+`;
+
+export const WriterName = styled.div`
+  align-self: center;
+`;
+
+export const Box = styled.div`
+  display: flex;
+`;
