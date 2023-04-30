0045c59c090a419b84e1c2c1dbc07c3a485cf14f
KwonSuyeon <commetoi_yeoni@naver.com>
2023-04-30 15:08:52 +0900

    feat: MainList 생성

---
+++ src/constant/MainPage/Main.js
@@ -0,0 +1,15 @@
+export const MAIN_CATEGORY_LIST = [
+  { icon: "/image/icon/designIcon.svg", title: "디자인" },
+  { icon: "/image/icon/ArtIcon.svg", title: "순수미술" },
+  { icon: "/image/icon/MusicIcon.svg", title: "음악" },
+  { icon: "/image/icon/dancingIcon.svg", title: "무용" },
+  { icon: "/image/icon/AthleticIcon.svg", title: "체육" },
+  { icon: "/image/icon/etcIcon.svg", title: "기타" },
+];
+
+export const MAIN_TAG_LIST = [
+  { title: "# UI / UX" },
+  { title: "# 바이올린" },
+  { title: "# 성악" },
+  { title: "# 그래픽" },
+];
