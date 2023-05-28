65c258acaec1a73b1b01d7062e156eae4aeee858
KwonSuyeon <commetoi_yeoni@naver.com>
2023-05-29 02:31:17 +0900

    feat: studyForm list 추가

---
+++ src/constant/StudyPage/StudyList.js
@@ -0,0 +1,27 @@
+export const STUDY_REGION_LIST = [
+  { data: "서울" },
+  { data: "경기" },
+  { data: "인천" },
+  { data: "부산" },
+  { data: "대구" },
+  { data: "대전" },
+  { data: "광주" },
+  { data: "강원" },
+  { data: "경북" },
+  { data: "전북" },
+  { data: "충북" },
+  { data: "세종" },
+  { data: "울산" },
+  { data: "경남" },
+  { data: "전남" },
+  { data: "충남" },
+  { data: "제주" },
+  { data: "해외" },
+];
+
+export const STUDY_CATEGORY_LIST = [
+  { category: "음악" },
+  { category: "디자인" },
+  { category: "미술" },
+  { category: "실용음악" },
+];
