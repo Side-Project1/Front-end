cf0e65174cc81326fa1eaa041e180a65a4aa4c10
KwonSuyeon <commetoi_yeoni@naver.com>
2023-05-28 13:07:02 +0900

    feat: promotion api 추가

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -7,3 +7,9 @@ export const GET_REFRESHTOKEN = "/atuh/refreshtoken/{userId}";
 export const GET_CONFIRM_EMAIL = "/api/v1/email/confirm";
 export const POST_FIND_EMAIL = "/api/v1/email/findId";
 export const POST_SEND_EMAIL = "/api/v1/email/send";
+
+/*Promotion*/
+export const GET_PROMOTION_FORM = "/api/v1/prom/1";
+export const POST_PROMOTION_FORM = "/api/v1/prom";
+export const PUT_PROMOTION_FORM = "/api/v1/prom/1";
+export const DELETE_PROMOTION_FORM = "/api/v1/prom/1";
