541a6bd1798e65d3699225093a13b3a9825f1b0c
KwonSuyeon <commetoi_yeoni@naver.com>
2023-05-28 16:55:53 +0900

    feat: promotion post 구현

--- src/constant/promotionPage/PromotionForm.jsx
+++ src/constant/promotionPage/PromotionForm.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import axios from "axios";
+import { v4 as uuidv4 } from "uuid";
 import {
   BorderKeyword,
   InputWrapKeyword,
@@ -15,123 +17,136 @@ import {
   PromotionTextWrap,
 } from "./promotionFormstyle";
 
-
-import { PromotionRegisterButton, PromotionRegisterButtonWrap } from "./Promotionstyle";
-
+import {
+  PromotionRegisterButton,
+  PromotionRegisterButtonWrap,
+} from "./Promotionstyle";
+import { API_URL } from "../../config/constant";
+import { POST_PROMOTION_FORM } from "../../api/apiUrl";
 
 const PromotionForm = () => {
   const [plus, setPlus] = useState([]);
-  const [store, setStore] = useState([])
   const [write, setWrite] = useState("");
   const [title, setTitle] = useState("");
-  const [introduction, setIntroduction] = useState("");
+  const [contents, setContents] = useState("");
+  const [sub_category, setSubCategory] = useState([]);
 
-  // const handleRegister = async () => {
-  //   const requestData = {
-  //     "contents": introduction,
-  //     "sub_category": [1],
-  //     "title": title,
-  //   }
-  //   //   await axios.post('http://13.209.81.190:8080/api/v1/prom', requestData,
-  //       {
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           "authorization": "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3NDI0ODAyMS01MTA3LTQwYzAtOWJkZC0yZDNjMmQxYTNmMmUiLCJpYXQiOjE2ODQ0MDQwNDQsImV4cCI6MTY4NDQwNTg0NH0.Vfv3yn4lnjiCQm2J-XOh-mGkBbT83vkADIm4o4iWRsKXB_XvC7cceC_Cg-2Sd6R9kFp70VRXvBJMFQmsZWsc4g"
-  //         }
-  //       },
-  //     )
-  //       .then(response => {
-  //         const postData = response.data
-  //         setStore(postData)
-  //         console.log('성공입니다')
-  //       })
-  //       .catch(error => {
-  //         console.log(error)
-  //         console.log('실패했습니다')
-  //       })
-  //     console.log(store)
-  //   }
-
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    // 여기서 로컬 스토리지에 저장되어 있는 토큰을 가져오는데....
+    const token = localStorage.getItem("accessToken");
+    try {
+      const response = await axios.post(
+        `${API_URL}${POST_PROMOTION_FORM}`,
+        {
+          title,
+          contents,
+          sub_category,
+        },
+        // headers에도 보내고 있음
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      console.log("게시글이 등록되었습니다.");
+      console.log(response.data);
+      setTitle("");
+      setContents("");
+      setSubCategory([]);
+    } catch (error) {
+      console.log("게시글 등록 실패");
+      console.error(error.response.data.data);
+      console.error(error.response.data.message);
+    }
+  };
 
   return (
-    <PromotionFormWrap>
-      <PromotionProfileWrap>
-        <ProfileTitle>홍보게시판</ProfileTitle>
-      </PromotionProfileWrap>
-      <PromotionTextWrap>
-        <PromotioTextForm>
-          <PromotionInputWrapper>
-            <span>제목</span>
-            <PromotionInput1
-              placeholder="제목을 입력해주세요"
-              // value={store.data.title}
-              onChange={(e) => setTitle(e.target.value)}
-            />
-          </PromotionInputWrapper>
-          <PromotionInputWrapper>
-            <span>키워드</span>
-            <InputWrapKeyword>
-              <KeyWordInputflex>
-                <PromotionInput3
-                  onChange={(e) => {
-                    setWrite(e.target.value);
-                  }}
-                  placeholder="키워드를 입력해주세요"
-
-                />
-                <PromotionInput3Button
-                  variant="outlined"
-                  onClick={() => {
-                    if (write.trim() !== "") {
-                      const copy = [...plus];
-                      copy.push(write.trim());
-                      setPlus(copy);
-                    }
-                  }}
-                >
-                  추가
-                </PromotionInput3Button>
-              </KeyWordInputflex>
-              <div>
-                {plus.map((e, i) => {
-                  return (
-                    <>
-                      <BorderKeyword>
-                        <span>{plus[i]}</span>
-                      </BorderKeyword>
-                      <button
-                        onClick={() => {
-                          let copy = [...plus];
-                          copy.splice(i, 1);
-                          setPlus(copy);
-                        }}
-                      >
-                        x
-                      </button>
-                    </>
-                  );
-                })}
-              </div>
-            </InputWrapKeyword>
-          </PromotionInputWrapper>
-          <PromotionInputWrapper>
-            <span>자기소개</span>
-            <PromotionInput2
-              id="filled-textarea"
-              label="한줄 소개"
-              placeholder="자신을 어필 할 수 있는 소개를 해주세요!"
-              multiline="true"
-              value={introduction}
-              onChange={(e) => setIntroduction(e.target.value)}
-            />
-          </PromotionInputWrapper>
-          <PromotionRegisterButtonWrap>
-            <PromotionRegisterButton>등록</PromotionRegisterButton>
-            <PromotionRegisterButton>취소</PromotionRegisterButton>
-          </PromotionRegisterButtonWrap>
-        </PromotioTextForm>
-      </PromotionTextWrap>
-    </PromotionFormWrap>
+    <form onSubmit={handleSubmit}>
+      <PromotionFormWrap>
+        <PromotionProfileWrap>
+          <ProfileTitle>홍보게시판</ProfileTitle>
+        </PromotionProfileWrap>
+        <PromotionTextWrap>
+          <PromotioTextForm>
+            <PromotionInputWrapper>
+              <span>제목</span>
+              <PromotionInput1
+                placeholder="제목을 입력해주세요"
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </PromotionInputWrapper>
+            <PromotionInputWrapper>
+              <span>키워드</span>
+              <InputWrapKeyword>
+                <KeyWordInputflex>
+                  <PromotionInput3
+                    id="sub_category"
+                    value={write}
+                    onChange={(e) => {
+                      setWrite(e.target.value);
+                    }}
+                    placeholder="키워드를 입력해주세요"
+                  />
+                  <PromotionInput3Button
+                    variant="outlined"
+                    onClick={() => {
+                      if (write.trim() !== "") {
+                        setPlus((prevPlus) => [...prevPlus, write.trim()]);
+                        setWrite("");
+                      }
+                    }}
+                  >
+                    추가
+                  </PromotionInput3Button>
+                </KeyWordInputflex>
+                <div>
+                  {plus.map((e, i) => {
+                    return (
+                      <>
+                        <BorderKeyword key={i}>
+                          <span>{plus[i]}</span>
+                        </BorderKeyword>
+                        <button
+                          onClick={() => {
+                            let copy = [...plus];
+                            copy.splice(i, 1);
+                            setPlus(copy);
+                          }}
+                        >
+                          x
+                        </button>
+                      </>
+                    );
+                  })}
+                </div>
+              </InputWrapKeyword>
+            </PromotionInputWrapper>
+            <PromotionInputWrapper>
+              <span>자기소개</span>
+              <PromotionInput2
+                label="한줄 소개"
+                placeholder="자신을 어필 할 수 있는 소개를 해주세요!"
+                multiline="true"
+                id="contents"
+                value={contents}
+                onChange={(e) => setContents(e.target.value)}
+              />
+            </PromotionInputWrapper>
+            <PromotionRegisterButtonWrap>
+              <PromotionRegisterButton type="submit">
+                등록
+              </PromotionRegisterButton>
+              <PromotionRegisterButton>취소</PromotionRegisterButton>
+            </PromotionRegisterButtonWrap>
+          </PromotioTextForm>
+        </PromotionTextWrap>
+      </PromotionFormWrap>
+    </form>
   );
 };
 export default PromotionForm;
