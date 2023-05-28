e70b8510603d89f27c82f723567cc7ca410fa349
KwonSuyeon <commetoi_yeoni@naver.com>
2023-05-29 01:03:18 +0900

    fix: 임시 주석처리

--- src/components/PromotionCard.js
+++ src/components/PromotionCard.js
@@ -10,11 +10,29 @@ import Profile from "../assets/images/profileicon.png";
 import { ProfileCardImg } from "../constant/promotionPage/Promotionstyle";
 import { useCallback, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import axios from "axios"; // axios를 import 합니다.
+import { API_URL } from "../config/constant";
+import { GET_ALL_PROMOTION_FORM } from "../api/apiUrl";
+
 const PromotionCard = (props) => {
   const { data, ...restProps } = props;
   const navigate = useNavigate();
 
-  console.log(data)
+  // const fetchPromotionData = useCallback(async () => {
+  //   try {
+  //     const response = await axios.get(`${API_URL}${GET_ALL_PROMOTION_FORM}`, {
+  //       params: data,
+  //     });
+  //     console.log(response.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, [data]);
+
+  // useEffect(() => {
+  //   fetchPromotionData();
+  // }, [fetchPromotionData]);
+
   return (
     <CardWrap>
       <ProfileCardImg src={Profile} />
@@ -23,9 +41,7 @@ const PromotionCard = (props) => {
       <CategoryWrap>
         <CategoryBox>{data?.contents}</CategoryBox>
       </CategoryWrap>
-      <CardDetailText>
-        {data?.user_id} | 부산
-      </CardDetailText>
+      <CardDetailText>{data?.user_id} | 부산</CardDetailText>
     </CardWrap>
   );
 };
