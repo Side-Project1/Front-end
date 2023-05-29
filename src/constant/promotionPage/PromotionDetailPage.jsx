925b750f24036c24a1f725f657cd44a5a46a3aac
KwonSuyeon <commetoi_yeoni@naver.com>
2023-05-29 23:42:42 +0900

    feat: prettier 적용

--- src/constant/promotionPage/PromotionDetailPage.jsx
+++ src/constant/promotionPage/PromotionDetailPage.jsx
@@ -1,38 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { DetailFormWrap} from "./promotionDetailstyle";
+import { DetailFormWrap } from "./promotionDetailstyle";
 import { useParams } from "react-router-dom";
 import PromotionDetailTextForm from "../../components/feature/PromotionDetailUI";
 import axios from "axios";
-
+import { API_URL } from "../../config/constant";
+import { GET_PROMOTION_FORM } from "../../api/apiUrl";
 
 const PromotionDetailPage = () => {
-
   const { id } = useParams();
   const [plus, setPlus] = useState([]);
   const [detailData, setDetailData] = useState([]);
 
-
-
   useEffect(() => {
-    axios.get(`http://13.209.81.190:8080/api/v1/prom/${id}`)
-      .then(response => {
-        const responseData = response.data
+    axios
+      .get(`${API_URL}${GET_PROMOTION_FORM}${id}`)
+      .then((response) => {
+        const responseData = response.data;
         setDetailData(responseData);
-        console.log('성공했습니다')
-
+        console.log("성공했습니다");
       })
-      .catch(error => {
-        console.log(error, '실패하였습니다')
-      })
-  }, [id])
+      .catch((error) => {
+        console.log(error, "실패하였습니다");
+      });
+  }, [id]);
 
   return (
     <DetailFormWrap>
       <PromotionDetailTextForm
-        value={id} plus={plus} setPlus={setPlus} data={detailData} setDetailData={setDetailData} />
+        value={id}
+        plus={plus}
+        setPlus={setPlus}
+        data={detailData}
+        setDetailData={setDetailData}
+      />
     </DetailFormWrap>
   );
 };
 
 export default PromotionDetailPage;
-
