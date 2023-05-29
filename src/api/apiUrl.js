d4beb986645f4bd1dc692cfd82393f31903bedcb
KwonSuyeon <commetoi_yeoni@naver.com>
2023-05-29 18:28:04 +0900

    feat: api 추가

--- src/api/apiUrl.js
+++ src/api/apiUrl.js
@@ -19,9 +19,13 @@ export const DELETE_PROMOTION_FORM = "/api/v1/prom/{promotionId}";
 export const GET_ALL_STUDY_FORM = "/api/v1/study";
 export const GET_STUDY_FORM = "/api/v1/prom/{promotionId}";
 export const POST_STUDY_FORM = "/api/v1/study";
+/*Study Comment*/
+export const GET_STUDY_FORM_COMMENT = "/api/v1/study/comment";
 export const POST_STUDY_FORM_COMMENT = "/api/v1/study/comment";
+export const PUT_STUDY_FORM_COMMENT = "/api/v1/study/comment";
+export const DELETE_STUDY_FORM_COMMENT = "/api/v1/study/comment";
+/*Study DeadLine*/
 export const POST_STUDY_FORM_DEADLINE = "/api/v1/study/{studyId}/dead";
-
 /*Study Scrap*/
 export const GET_STUDY_FORM_SCRAP = "/api/v1/study/scrap";
 export const POST_STUDY_FORM_SCRAP = "/api/v1/study/scrap/{scrapId}";
